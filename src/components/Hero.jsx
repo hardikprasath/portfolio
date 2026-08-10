7ed1da46bb46da93f06731008a629888e0aebb07
prasath <prasath51542@gmail.com>
2026-08-10 21:51:16 +0530

    Drive link will be changes

--- src/components/Hero.jsx
+++ src/components/Hero.jsx
@@ -25,7 +25,7 @@ const Hero = () => {
         </button>
         <button
           className="bg-gradient-to-r from-pink-500 to-yellow-500 text-white block transform transition-transform duration-300 hover:scale-105 px-4 py-2 rounded-full">
-          <a href="https://drive.google.com/file/d/1OZrpw47NnF5ao24WNdiNOcoZpG91safo/view?usp=sharing">Resume</a>
+          <a href="https://drive.google.com/file/d/1Ac8zaXoknjEC-U1U4gTG42GDaJTHYOD-/view?usp=sharing">Resume</a>
         </button>
       </div>
 
